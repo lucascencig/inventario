d6f30770c759f24c46266f7437a160c08392825e
lucascencig <cenciglucas@gmail.com>
2024-05-15 18:04:19 -0300

    update user without finish

--- vistas/home.php
+++ vistas/home.php
@@ -1,4 +1,4 @@
 <div class="container is-fluid">
   <h1 class="title">Home</h1>
-  <h2 class="subtitle">¡Bienvenido usuario!</h2>
+  <h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre'] ?> <?php echo $_SESSION['apellido'] ?>!</h2>
 </div>
